9cbe86d82144f9b836075966d12747e5686617cf
Diego <diegotaveras2@outlook.com>
2023-02-22 11:42:16 -0500

    add new appointment modal form

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
-import React from 'react';
-import {Text, SafeAreaView, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Text, SafeAreaView, StyleSheet, Pressable} from 'react-native';
 
+import Form from './src/components/Form';
 function App() {
+  const [showModal, setShowModal] = useState(false);
   return (
-    <SafeAreaView>
-      <Text style={styles.title}> Hello World</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        {' '}
+        Appointment Manager {'\n'}
+        <Text style={styles.boldTitle}>Vet</Text>
+      </Text>
+      <Pressable style={styles.newApptBtn} onPress={() => setShowModal(true)}>
+        <Text style={styles.apmtTextBtn}>New Appointment</Text>
+      </Pressable>
+      <Form showModal={showModal} setShowModal={setShowModal} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F3F4F6',
+    flex: 1,
+  },
   title: {
     textAlign: 'center',
     fontSize: 30,
     color: '#374151',
     fontWeight: '600',
+  },
+  boldTitle: {
+    fontWeight: '900',
+    color: '#6d28d9',
+  },
+  newApptBtn: {
+    backgroundColor: '#6D28D9',
+    padding: 15,
+    marginTop: 30,
+    marginHorizontal: 20,
+    borderRadius: 10,
+  },
+  apmtTextBtn: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 20,
+    fontWeight: '900',
+    textTransform: 'uppercase',
   },
 });
 
---
+++ src/components/Form.js
@@ -0,0 +1,172 @@
+import React, {useState} from 'react';
+import {
+  Modal,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  View,
+  ScrollView,
+  Pressable,
+} from 'react-native';
+import DatePicker from 'react-native-date-picker';
+const Form = ({showModal, setShowModal}) => {
+  const [pacient, setPacient] = useState('');
+  const [owner, setOwner] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
+  const [symptomes, setSymptomes] = useState('');
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <Modal animationType="slide" visible={showModal}>
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <Text style={styles.title}>
+            New <Text style={styles.boldTitle}>Appointment</Text>
+          </Text>
+          <Pressable
+            onLongPress={() => setShowModal(false)}
+            style={styles.cancelBtn}>
+            <Text style={styles.cancelTxt}>X Cancel</Text>
+          </Pressable>
+          <View style={styles.field}>
+            <Text style={styles.label}>Pacient Name</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Pacient Name"
+              placeholderTextColor={'#666'}
+              value={pacient}
+              onChangeText={setPacient}
+            />
+          </View>
+
+          <View style={styles.field}>
+            <Text style={styles.label}>Owner Name</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Owner Name"
+              placeholderTextColor={'#666'}
+              value={owner}
+              onChangeText={setOwner}
+            />
+          </View>
+
+          <View style={styles.field}>
+            <Text style={styles.label}>Email</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="email-address"
+              placeholder="Email"
+              placeholderTextColor={'#666'}
+              value={email}
+              onChangeText={setEmail}
+            />
+          </View>
+
+          <View style={styles.field}>
+            <Text style={styles.label}>Phone Number</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="phone-pad"
+              placeholder="Phone Number"
+              placeholderTextColor={'#666'}
+              value={number}
+              onChangeText={setNumber}
+              maxLength={10}
+            />
+          </View>
+
+          <View style={styles.field}>
+            <Text style={styles.label}>Date</Text>
+            <View style={styles.dateContainer}>
+              <DatePicker
+                date={date}
+                mode="date"
+                onDateChange={date => setDate(date)}
+              />
+            </View>
+          </View>
+
+          <View style={styles.field}>
+            <Text style={styles.label}>symptoms</Text>
+            <TextInput
+              style={[styles.input, styles.symptomsInput]}
+              placeholder="Symptoms"
+              placeholderTextColor={'#666'}
+              value={symptomes}
+              onChangeText={setSymptomes}
+              multiline={true}
+              numberOfLines={4}
+            />
+          </View>
+
+          <Pressable style={styles.newApptmBtn}>
+            <Text style={styles.newApptmTxt}>Add Patient</Text>
+          </Pressable>
+        </ScrollView>
+      </SafeAreaView>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {backgroundColor: '#6D28D9', flex: 1},
+  title: {
+    fontSize: 30,
+    fontWeight: '600',
+    textAlign: 'center',
+    marginTop: 30,
+    color: '#FFF',
+  },
+  boldTitle: {
+    fontWeight: '900',
+  },
+  cancelBtn: {
+    marginVertical: 30,
+    backgroundColor: '#5827A4',
+    marginHorizontal: 30,
+    padding: 15,
+    borderRadius: 10,
+  },
+  cancelTxt: {
+    color: '#FFF',
+    fontWeight: '900',
+    fontSize: 16,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+  },
+  field: {marginTop: 10, marginHorizontal: 30, marginBottom: 10},
+  label: {
+    color: '#FFF',
+    marginBottom: 10,
+    marginTop: 15,
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  input: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
+  },
+  symptomsInput: {height: 100},
+  dateContainer: {
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+  },
+  newApptmBtn: {
+    marginVertical: 50,
+    backgroundColor: '#F59E0B',
+    paddingVertical: 15,
+    marginHorizontal: 30,
+    borderRadius: 10,
+  },
+  newApptmTxt: {
+    textAlign: 'center',
+    color: '#5827A4',
+    textTransform: 'uppercase',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+});
+export default Form;
